365b859439f4c5a11356940553569c08d9e249ff
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-03-24 15:23:44 +0100

    color interface

--- src/CodeWorld/Tasks/Types.hs
+++ src/CodeWorld/Tasks/Types.hs
@@ -6,12 +6,37 @@ module CodeWorld.Tasks.Types (
   Vector,
 
   Color,
+  Colour,
   TextStyle,
   Font,
 
   red,
   green,
   yellow,
+  black,
+  white,
+  blue,
+  orange,
+  brown,
+  pink,
+  purple,
+  gray,
+  grey,
+  mixed,
+  lighter,
+  light,
+  darker,
+  dark,
+  brighter,
+  bright,
+  duller,
+  dull,
+  translucent,
+  assortedColors,
+  hue,
+  saturation,
+  luminosity,
+  alpha,
   ) where
 
 
@@ -45,10 +70,92 @@ data Color
   = Yellow
   | Green
   | Red
+  | Blue
+  | Orange
+  | Brown
+  | Pink
+  | Purple
+  | Grey
+  | White
+  | Black
+  | Modified Color
+  | Mixed [Color]
+  | RGB Double Double Double
+  | HSL Double Double Double
+
   deriving (Eq,Ord,Show,Generic,NFData)
 
+type Colour = Color
 
-green, red, yellow :: Color
+
+green, red, yellow, black, white, blue, orange, brown, pink, purple, gray, grey :: Color
 yellow = Yellow
 green  = Green
 red    = Red
+black = Black
+white = White
+blue = Blue
+orange = Orange
+brown = Brown
+pink = Pink
+purple = Purple
+grey = Grey
+gray = grey
+
+
+oneLayer :: Color -> Color
+oneLayer (Modified c) = Modified c
+oneLayer c            = Modified c
+
+
+mixed :: [Color] -> Color
+mixed = Mixed
+
+lighter :: Double -> Color -> Color
+lighter _ = oneLayer
+
+light :: Color -> Color
+light = oneLayer
+
+darker :: Double -> Color -> Color
+darker _ = oneLayer
+
+dark :: Color -> Color
+dark = oneLayer
+
+brighter :: Double -> Color -> Color
+brighter _ = oneLayer
+
+bright :: Color -> Color
+bright = oneLayer
+
+duller :: Double -> Color -> Color
+duller _ = oneLayer
+
+dull :: Color -> Color
+dull = oneLayer
+
+translucent :: Color -> Color
+translucent = oneLayer
+
+assortedColors :: [Color]
+assortedColors = iterate Modified red
+
+
+-- Don't know what to do with these. Never seen anyone use them,
+-- but could be problematic if this is used in program logic.
+
+hue :: Color -> Double
+hue (HSL h _ _) = h
+hue _ = 0.5
+
+saturation :: Color -> Double
+saturation (HSL _ s _) = s
+saturation _ = 0.5
+
+luminosity :: Color -> Double
+luminosity (HSL _ _ l) = l
+luminosity _ = 0.5
+
+alpha :: Color -> Double
+alpha _ = 1
