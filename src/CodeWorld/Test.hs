890e93f17dcc55126533aefa28646e73627e32d3
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-03-24 16:18:24 +0100

    central export file for tests

---
+++ src/CodeWorld/Test.hs
@@ -0,0 +1,14 @@
+
+module CodeWorld.Test (
+  module Abstract,
+  module Normalize,
+  module Relative,
+  module Solution,
+) where
+
+
+import CodeWorld.Test.Abstract as Abstract
+import CodeWorld.Test.Normalize as Normalize
+import CodeWorld.Test.Relative as Relative
+import CodeWorld.Test.Solution as Solution
+
