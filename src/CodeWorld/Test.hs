4d90120190c4f929635eb1f256f96c24b0660fa6
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-03-24 16:26:55 +0100

    add API module to test import

--- src/CodeWorld/Test.hs
+++ src/CodeWorld/Test.hs
@@ -1,12 +1,14 @@
 
 module CodeWorld.Test (
   module Abstract,
+  module API,
   module Normalize,
   module Relative,
   module Solution,
 ) where
 
 
+import CodeWorld.Tasks.API as API
 import CodeWorld.Test.Abstract as Abstract
 import CodeWorld.Test.Normalize as Normalize
 import CodeWorld.Test.Relative as Relative
