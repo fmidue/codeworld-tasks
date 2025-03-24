0192b63f13ec5893cce9b54e2943127e6de24ad7
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-03-24 10:28:31 +0100

    annotate picture comparison functions

--- src/CodeWorld/Tasks/Solution.hs
+++ src/CodeWorld/Tasks/Solution.hs
@@ -3,6 +3,7 @@ module CodeWorld.Tasks.Solution (
   Spec(..),
   spec,
   specElems,
+  containsElems,
   containsExactElems,
   specPosition,
   evaluate,
@@ -29,25 +30,34 @@ spec :: [Components -> Bool] -> PredSpec
 spec = PredSpec
 
 
+-- Use a predicate on the list of sub images
 specElems :: ([NormalizedPicture] -> Bool) -> Components -> Bool
-specElems f (Components (e,_)) = f e
+specElems f (Components (ps,_)) = f ps
 
 
+-- Input contains exactly these sub pictures
 containsExactElems :: [NormalizedPicture] -> Components -> Bool
-containsExactElems a = specElems (\c -> all (`elem` c) a)
+containsExactElems ps = specElems (all (`elem` ps))
 
+-- Input contains at least these sub pictures
+containsElems :: [NormalizedPicture] -> Components -> Bool
+containsElems ps = specElems (\c -> all (`elem` c) ps)
 
+-- Use a predicate on the list of relative positionings
 specPosition :: ([RelativePicSpec] -> Bool) -> Components -> Bool
 specPosition f (Components (_,rP)) = f rP
 
 
+-- Evaluate all of the given predicates
 evaluate :: PredSpec -> NormalizedPicture -> Bool
 evaluate (PredSpec fs) pic = all (\f -> f $ toRelative pic) fs
 
 
+-- Input is exactly this relative picture
 isExactly :: RelativePicSpec -> Components -> Bool
 isExactly a = specPosition (==[a])
 
 
+-- Input contains at least this relative picture
 has :: RelativePicSpec -> Components -> Bool
 has a = specPosition (a `elem`)
