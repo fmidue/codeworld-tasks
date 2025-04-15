51ae3773d407cabafcdea372e2b291263425fea0
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-04-15 10:07:55 +0200

    broaden conditional and make function types clearer

--- src/CodeWorld/Test/Solution.hs
+++ src/CodeWorld/Test/Solution.hs
@@ -13,7 +13,7 @@ module CodeWorld.Test.Solution (
   (<||>),
   option,
   options,
-  ifPresentThen,
+  ifThen,
   ) where
 
 
@@ -29,67 +29,67 @@ import CodeWorld.Test.Relative (
 
 --data Spec = Only Components | ReqAndOpt (Components,Components)
 
-type PredSpec = [Components -> Bool]
+type PicPredicate = Components -> Bool
 
 
 -- At least one of arbitrarily many predicates evaluates to True
-options :: [Components -> Bool] -> Components -> Bool
+options :: [PicPredicate] -> PicPredicate
 options ps c = any (\p -> p c) ps
 
 
 -- At least one of two predicates evaluates to True
-(<||>) :: (Components -> Bool) -> (Components -> Bool) -> Components -> Bool
+(<||>) :: PicPredicate -> PicPredicate -> PicPredicate
 (<||>) p q c = p c || q c
 
 
 -- Alias for (<||>)
-option :: (Components -> Bool) -> (Components -> Bool) -> Components -> Bool
+option :: PicPredicate -> PicPredicate -> PicPredicate
 option = (<||>)
 
 
 -- Use a predicate on the list of sub images
-specElems :: ([NormalizedPicture] -> Bool) -> Components -> Bool
+specElems :: ([NormalizedPicture] -> Bool) -> PicPredicate
 specElems f (Components (ps,_)) = f ps
 
 
 -- Input contains exactly these sub pictures
-containsExactElems :: [NormalizedPicture] -> Components -> Bool
+containsExactElems :: [NormalizedPicture] -> PicPredicate
 containsExactElems ps = specElems (all (`elem` ps))
 
 -- Input contains at least this sub picture
-containsElem :: NormalizedPicture -> Components -> Bool
+containsElem :: NormalizedPicture -> PicPredicate
 containsElem p = specElems (any (`contains` p))
 
 -- Input contains at least these sub pictures
-containsElems :: [NormalizedPicture] -> Components -> Bool
+containsElems :: [NormalizedPicture] -> PicPredicate
 containsElems ps = specElems (any (or . (\c -> map (c `contains`) ps)))
 
 
--- run a predicate on the input only if specific sub pictures are present
-ifPresentThen :: [NormalizedPicture] -> (Components -> Bool) -> Components -> Bool
-ifPresentThen ps f comp = not (containsElems ps comp) || f comp
+-- run a predicate on the input only if another succeeded already
+ifThen :: PicPredicate -> PicPredicate -> PicPredicate
+ifThen f g comp = not (f comp) || g comp
 
 
 -- Use a predicate on the list of relative positions
-specPosition :: ([RelativePicSpec] -> Bool) -> Components -> Bool
+specPosition :: ([RelativePicSpec] -> Bool) -> PicPredicate
 specPosition f (Components (_,rP)) = f rP
 
 
 -- Evaluate all of the given predicates on the student submission
-evaluateSpec :: PredSpec -> Picture -> Bool
+evaluateSpec :: [PicPredicate] -> Picture -> Bool
 evaluateSpec fs pic = all (\f -> f $ toRelative $ toInterface pic) fs
 
 
 -- Input is exactly this relative picture
-isExactly :: RelativePicSpec -> Components -> Bool
+isExactly :: RelativePicSpec -> PicPredicate
 isExactly a = specPosition (==[a])
 
 
 -- Input contains at least this relative picture
-hasExactly :: RelativePicSpec -> Components -> Bool
+hasExactly :: RelativePicSpec -> PicPredicate
 hasExactly a = specPosition (a `elem`)
 
 
 -- Input contains elements satisfying the given spatial predicate
-hasBroadly :: (RelativePicSpec -> Bool) -> Components -> Bool
+hasBroadly :: (RelativePicSpec -> Bool) -> PicPredicate
 hasBroadly f = specPosition (any f)
