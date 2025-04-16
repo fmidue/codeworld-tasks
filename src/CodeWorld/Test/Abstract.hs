585b2d68fec1c81e29bd88cf9b37203408c225d0
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-04-16 17:16:46 +0200

    add tutor shorthands for scaling closes #21

--- src/CodeWorld/Test/Abstract.hs
+++ src/CodeWorld/Test/Abstract.hs
@@ -29,3 +29,21 @@ rotatedThreeQuarters = rotated (3*pi/2)
 
 rotatedUpToFull :: Drawable a => a -> a
 rotatedUpToFull = rotated 5
+
+smaller :: Drawable a => a -> a
+smaller = dilated 0.5
+
+larger :: Drawable a => a -> a
+larger = dilated 2
+
+smallerX :: Drawable a => a -> a
+smallerX = scaled 0.5 1
+
+largerX :: Drawable a => a -> a
+largerX = scaled 2 1
+
+smallerY :: Drawable a => a -> a
+smallerY = scaled 1 0.5
+
+largerY :: Drawable a => a -> a
+largerY = scaled 1 2
