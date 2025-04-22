63c531d062a290ca8825a92a504fa7092cbdf9f9
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-04-22 11:17:37 +0200

    expose cse test

--- src/CodeWorld/Test.hs
+++ src/CodeWorld/Test.hs
@@ -2,6 +2,7 @@
 module CodeWorld.Test (
   module Abstract,
   module API,
+  module Compare,
   module Normalize,
   module Relative,
   module Solution,
@@ -14,5 +15,6 @@ import CodeWorld.Tasks.Types as Types
 import CodeWorld.Test.Abstract as Abstract
 import CodeWorld.Test.Normalize as Normalize
 import CodeWorld.Test.Relative as Relative
+import CodeWorld.Tasks.Compare as Compare
 import CodeWorld.Test.Solution as Solution
 
