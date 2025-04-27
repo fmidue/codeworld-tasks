781082468059a9a8128309fcc5fbad98d449fa1e
patritzenfeld <33265318+patritzenfeld@users.noreply.github.com>
2025-04-27 18:44:50 +0200

    finish up color operations for mixed

--- src/CodeWorld/Tasks/Types.hs
+++ src/CodeWorld/Tasks/Types.hs
@@ -1,5 +1,6 @@
 {-# language DeriveAnyClass #-}
 {-# language DeriveGeneric #-}
+{-# language ViewPatterns #-}
 
 module CodeWorld.Tasks.Types (
   Point,
@@ -180,10 +181,21 @@ clamp :: Double -> Double
 clamp = max 0 . min 1
 
 
+clampColor :: Color -> Color
+clampColor (RGBA r g b a) = RGBA (clamp r) (clamp g) (clamp b) (clamp a)
+clampColor (RGB r g b) = RGB (clamp r) (clamp g) (clamp b)
+clampColor (HSL h s l) = HSL (moduloTwoPi h) (clamp s) (clamp l)
+clampColor c = c
+
+
+moduloTwoPi :: Double -> Double
+moduloTwoPi x = x - fromInteger (floor (x / (2*pi))) * 2*pi
+
+
 hue :: Color -> Double
-hue (HSL h _ _) = h
+hue (HSL (moduloTwoPi -> h) _ _) = h
 hue (RGBA r g b _) = hue (RGB r g b)
-hue (RGB r g b)
+hue (clampColor -> RGB r g b)
   | hi - lo < epsilon = 0
   | r == hi && g >= b = (g - b) / (hi - lo) * pi / 3
   | r == hi = (g - b) / (hi - lo) * pi / 3 + 2 * pi
@@ -193,21 +205,23 @@ hue (RGB r g b)
     hi = max r (max g b)
     lo = min r (min g b)
     epsilon = 0.000001
+hue (Mixed cs) = hue $ mix cs
 hue c
   | isPredefined c = maybe (error "could not find color's hue!") fst3 $ lookup c colorHSL
   | otherwise = hue $ innerColor c
 
 
 saturation :: Color -> Double
-saturation (HSL _ s _) = s
+saturation (HSL _ (clamp -> s) _) = s
 saturation (RGBA r g b _) = saturation (RGB r g b)
-saturation (RGB r g b)
+saturation (clampColor -> RGB r g b)
   | hi - lo < epsilon = 0
   | otherwise = (hi - lo) / (1 - abs (hi + lo - 1))
   where
     hi = max r (max g b)
     lo = min r (min g b)
     epsilon = 0.000001
+saturation (Mixed cs) = saturation $ mix cs
 saturation (Bright c) = clamp $ saturation c + 0.25
 saturation (Brighter d c) = clamp $ saturation c + d
 saturation (Dull c) = clamp $ saturation c - 0.25
@@ -218,12 +232,13 @@ saturation c
 
 
 luminosity :: Color -> Double
-luminosity (HSL _ _ l) = l
+luminosity (HSL _ _ (clamp -> l)) = l
 luminosity (RGBA r g b _) = luminosity (RGB r g b)
-luminosity (RGB r g b) = (lo + hi) / 2
+luminosity (clampColor -> RGB r g b) = (lo + hi) / 2
   where
     hi = max r (max g b)
     lo = min r (min g b)
+luminosity (Mixed cs) = luminosity $ mix cs
 luminosity (Light c) = clamp $ luminosity c + 0.15
 luminosity (Lighter d c) = clamp $ luminosity c + d
 luminosity (Dark c) = clamp $ luminosity c - 0.15
@@ -233,7 +248,7 @@ luminosity c
   | otherwise = luminosity $ innerColor c
 
 alpha :: Color -> Double
-alpha (RGBA _ _ _ a)  = a
+alpha (RGBA _ _ _ (clamp -> a))  = a
 alpha (Translucent c) = alpha c / 2
 alpha _               = 1
 
@@ -252,3 +267,38 @@ colorHSL =
   , (Pink,(5.76,0.75,0.75))
   , (Brown,(0.52,0.6,0.4))
   ]
+
+
+-- taken and slightly adapted from codeworld-api
+mix :: [Color] -> Color
+mix = go 0 0 0 0 0
+  where
+    go rr gg bb aa n (c:cs) = let (r, g, b, a) = toRGBA c in
+      go (rr + r * r * a) (gg + g * g * a) (bb + b * b * a) (aa + a) (n + 1) cs
+    go rr gg bb aa n []
+      | aa == 0 = RGBA 0 0 0 0
+      | otherwise = RGBA (sqrt (rr / aa)) (sqrt (gg / aa)) (sqrt (bb / aa)) (aa / n)
+
+
+-- taken and slightly adapted from codeworld-api
+toRGBA :: Color -> (Double,Double,Double,Double)
+toRGBA (clampColor -> RGB r g b) = (r, b, g, 1)
+toRGBA (clampColor -> RGBA r b g a) = (r, b, g, a)
+toRGBA (clampColor -> HSL h s l) = (r, g, b, 1)
+  where
+    m1 = l * 2 - m2
+    m2
+      | l <= 0.5 = l * (s + 1)
+      | otherwise = l + s - l * s
+    r = convert m1 m2 (h / 2 / pi + 1 / 3)
+    g = convert m1 m2 (h / 2 / pi)
+    b = convert m1 m2 (h / 2 / pi - 1 / 3)
+    convert m1' m2' h'
+      | h' < 0 = convert m1' m2' (h' + 1)
+      | h' > 1 = convert m1' m2' (h' - 1)
+      | h' * 6 < 1 = m1' + (m2' - m1') * h' * 6
+      | h' * 2 < 1 = m2'
+      | h' * 3 < 2 = m1' + (m2' - m1') * (2 / 3 - h') * 6
+      | otherwise = m1'
+toRGBA (Translucent c) = let (r, g, b, a) = toRGBA c in (r, g, b, a/2)
+toRGBA c = toRGBA $ HSL (hue c) (saturation c) (luminosity c)
