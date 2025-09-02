f297a779597181ce05df62e2ce91de3e675cd727
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-09-02 16:48:57 +0200

    headings and explicit import lists in CodeWorld.Test

--- src/CodeWorld/Test.hs
+++ src/CodeWorld/Test.hs
@@ -4,23 +4,53 @@ Module exporting all functionality needed for running tests on student submissio
 -}
 
 module CodeWorld.Test (
+  -- * Helpers for Tests
   module Abstract,
+
+  -- * Abstract Representations of CodeWorld Types
   module AbsTypes,
+
+  -- * Animation Test Frame Generators
   module Animation,
+
+  -- * Drawable API
   module API,
+
+  -- * CSE detection
   module Sharing,
+
+  -- * Normalized Picture Type
+  N.NormalizedPicture,
   module Normalize,
-  R.Components,
+
+  -- * Spatial Relations
   module Relative,
+
+  -- * Test Interface
   module Solution,
+
+  -- * Functions for Point-based Shapes
+  V.wasTranslatedBy,
+  V.wasScaledBy,
+  V.wasRotatedBy,
+
+  -- * Concrete CodeWorld Types
   module Types,
+
+  -- * Math
   module VectorSpace,
+
+  -- * Strict Picture Type Internals
   Picture(..),
   ReifyPicture(..),
-  N.NormalizedPicture,
+
+  -- * Type Conversions
   normalize,
+  N.toConcretePicture,
   reduce,
   reduceNoOrder,
+
+  -- * Misc. Functions for Pictures
   hasInnerPicture,
   innerPicture,
   isIn,
@@ -32,30 +62,59 @@ import CodeWorld.Tasks.Types as Types
 import CodeWorld.Test.Abstract as Abstract
 import CodeWorld.Test.AbsTypes as AbsTypes
 import CodeWorld.Test.Animation as Animation
-import CodeWorld.Test.Normalize as Normalize hiding (
-  NormalizedPicture(..),
-  couldHaveTranslation,
-  getSubPictures,
-  stripTranslation,
+import CodeWorld.Test.Normalize as Normalize (
+  contains,
+  getColor,
+  getRotation,
+  getExactRotation,
+  getScalingFactors,
+  getExactScalingFactors,
+  getTranslation,
+  getExactTranslation,
+  getReflectionAngle,
+  getExactReflectionAngle,
+  getCircleRadius,
+  getExactCircleRadius,
+  getRectangleLengths,
+  getExactRectangleLengths,
+  getExactPointList,
   )
 import qualified CodeWorld.Test.Normalize as N
-import CodeWorld.Test.Relative as Relative hiding (
-  Alone,
+import CodeWorld.Test.Relative as Relative (
   Components,
-  Is,
-  (===),
-  toRelative,
+  RelativePicSpec,
+  SpatialQuery,
+  isSouthOf,
+  isNorthOf,
+  isWestOf,
+  isEastOf,
+  isSouthEastOf,
+  isSouthWestOf,
+  isNorthEastOf,
+  isNorthWestOf,
+  isBelow,
+  isAbove,
+  isLeftOf,
+  isRightOf,
+  atSamePosition,
   )
-import qualified CodeWorld.Test.Relative as R
 import CodeWorld.Sharing.Feedback as Sharing
 import CodeWorld.Test.Solution as Solution
-import CodeWorld.Tasks.VectorSpace as VectorSpace hiding (
-  atOriginWithOffset,
-  crossProduct,
-  dotProduct,
-  isRectangle,
-  rotationAngle,
-  sideLengths,
+import qualified CodeWorld.Tasks.VectorSpace as V
+import CodeWorld.Tasks.VectorSpace as VectorSpace (
+  Point,
+  Vector,
+  translatedPoint,
+  rotatedPoint,
+  reflectedPoint,
+  scaledPoint,
+  dilatedPoint,
+  vectorLength,
+  vectorDirection,
+  vectorSum,
+  vectorDifference,
+  scaledVector,
+  rotatedVector,
   )
 
 import CodeWorld.Tasks.Picture (
@@ -83,7 +142,7 @@ The result is a new syntax tree, which draws the same image,
 but was simplified and rearranged via `normalize`'s rules.
 -}
 reduce :: Picture -> Picture
-reduce = toConcretePicture . toInterface
+reduce = N.toConcretePicture . toInterface
 
 {- |
 Same as `reduce`,
