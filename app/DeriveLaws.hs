a56d56e1074ef824b8a9e59c45768051cb270930
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2026-05-05 18:15:30 +0200

    finish listing the constructors

--- app/DeriveLaws.hs
+++ app/DeriveLaws.hs
@@ -1,46 +1,82 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE OverloadedStrings #-}
 module DeriveLaws where
 
 
+import Data.Text (Text)
 import QuickSpec
 import Test.QuickCheck
 import Test.QuickCheck.Arbitrary ()
 import CodeWorld
 import CodeWorld.Test (normalize)
 
-
 sig :: Sig
 sig = signature
   [ con "rectangle" rectangle
+  , con "thickRectangle" thickRectangle
+  , con "solidRectangle" solidRectangle
   , con "circle" circle
+  , con "thickCircle" thickCircle
+  , con "solidCircle" solidCircle
   , con "arc" arc
+  , con "thickArc" thickArc
+  , con "sector" sector
   , con "blank" blank
-  , con "translated" $ translated
-  , con "scaled" $ scaled
+  , con "codeWorldLogo" codeWorldLogo
+  , con "coordinatePlane" coordinatePlane
+  , con "lettering" lettering
+  , con "styledLettering" styledLettering
+  , con "polyline" polyline
+  , con "thickPolyline" thickPolyline
+  , con "polygon" polygon
+  , con "thickPolygon" thickPolygon
+  , con "solidPolygon" solidPolygon
+  , con "curve" curve
+  , con "thickCurve" thickCurve
+  , con "closedCurve" closedCurve
+  , con "thickClosedCurve" thickClosedCurve
+  , con "solidClosedCurve" solidClosedCurve
+  , con "translated" translated
+  , con "scaled" scaled
+  , con "dilated" dilated
   , con "colored" colored
+  , con "rotated" rotated
+  , con "reflected" reflected
+  , con "clipped" clipped
+  , con "&" (&)
+  , con "pictures" pictures
   ]
 
 sigTypes :: Sig
 sigTypes = signature
   [ monoObserve @Picture
+  , monoObserve @[Picture]
   , mono @Color
+  , mono @Text
+  , mono @TextStyle
+  , mono @Font
   , vars ["x","y"] $ Proxy @Double
+  , vars ["s"] $ Proxy @Style
+  , vars ["f"] $ Proxy @Font
   , vars ["c"] $ Proxy @Color
   , vars ["p"] $ Proxy @Picture
+  , vars ["ps"] $ Proxy @[Picture]
+  , vars ["pts"] $ Proxy @[Point]
+  , withInferInstanceTypes
   ]
 
 sigBg :: Sig
 sigBg = background
   [ arith $ Proxy @Double
-  , con "*" $ (*) @Double
-  , con "-" $ (-) @Double
-  , con "negate" $ negate @Double
+--  , lists
+ -- , con "*" $ (*) @Double
+ -- , con "-" $ (-) @Double
+ -- , con "negate" $ negate @Double
   ]
 
 
-
 instance Observe () Picture Picture where
-  observe () t = normalize t
+  observe () = normalize
 
 
 instance Arbitrary Color where
@@ -49,22 +85,90 @@ instance Arbitrary Color where
     pure $ assortedColors !! n
 
 
+instance Arbitrary TextStyle where
+  arbitrary = elements
+    [ Plain
+    , Bold
+    , Italic
+    ]
+
+
+instance Arbitrary Font where
+  arbitrary = elements
+    [ SansSerif
+    , Serif
+    , Monospace
+    , Handwriting
+    , Fancy
+    , NamedFont "Font"
+    ]
+
+
 instance Arbitrary Picture where
   arbitrary = sized $ \n ->
     if n <= 1
-      then pure blank
+      then basic
       else frequency
-        [ (1, pure blank)
-        , (2, rectangle <$> arbitrary <*> arbitrary)
-        , (2, circle <$> arbitrary)
-        , (2, arc <$> arbitrary <*> arbitrary <*> arbitrary)
-        , (1, translated <$> arbitrary <*> arbitrary <*> decayArbitrary 2)
+        [ (1, translated <$> arbitrary <*> arbitrary <*> decayArbitrary 2)
         , (1, scaled <$> arbitrary <*> arbitrary <*> decayArbitrary 2)
+        , (1, dilated <$> arbitrary <*> decayArbitrary 2)
         , (1, colored <$> arbitrary <*> decayArbitrary 2)
+        , (1, rotated <$> arbitrary <*> decayArbitrary 2)
+        , (1, reflected <$> arbitrary <*> decayArbitrary 2)
+        , (1, clipped <$> arbitrary <*> arbitrary <*> decayArbitrary 2)
+        , (1, (&) <$> decayArbitrary 2 <*> decayArbitrary 2)
+        , (2, pictures <$> pictureList)
         ]
+
+
+basic :: Gen Picture
+basic = frequency
+  [ (1, pure blank)
+  , (1, pure codeWorldLogo)
+  , (1, pure coordinatePlane)
+  , (2, rectangle <$> arbitrary <*> arbitrary)
+  , (2, thickRectangle <$> positiveDouble <*> arbitrary <*> arbitrary)
+  , (2, solidRectangle <$> arbitrary <*> arbitrary)
+  , (2, circle <$> arbitrary)
+  , (2, solidCircle <$> arbitrary)
+  , (2, uncurry thickCircle <$> validThicknessRatio)
+  , (2, arc <$> arbitrary <*> arbitrary <*> arbitrary)
+  , (2, thickArc <$> positiveDouble <*> arbitrary <*> arbitrary <*> arbitrary)
+  , (2, sector <$> arbitrary <*> arbitrary <*> arbitrary)
+  , (2, lettering <$> arbitrary)
+  , (2, styledLettering <$> arbitrary <*> arbitrary <*> arbitrary)
+  , (2, polyline <$> arbitrary)
+  , (2, thickPolyline <$> positiveDouble <*> arbitrary)
+  , (2, polygon <$> arbitrary)
+  , (2, thickPolygon <$> positiveDouble <*> arbitrary)
+  , (2, solidPolygon <$> arbitrary)
+  , (2, curve <$> arbitrary)
+  , (2, thickCurve <$> positiveDouble <*> arbitrary)
+  , (2, closedCurve <$> arbitrary)
+  , (2, thickClosedCurve <$> positiveDouble <*> arbitrary)
+  , (2, solidClosedCurve <$> arbitrary)
+  ]
+
 
 decayArbitrary :: Arbitrary a => Int -> Gen a
 decayArbitrary n = scale (`div` n) arbitrary
+
+
+pictureList :: Gen [Picture]
+pictureList = do
+  m <- getSize
+  k  <- chooseInt (0, m)
+  vectorOf k (resize (m-k) arbitrary)
+
+
+positiveDouble :: Gen Double
+positiveDouble = arbitrary `suchThat` (>=0)
+
+validThicknessRatio :: Gen (Double, Double)
+validThicknessRatio = do
+  size <- arbitrary
+  thickness <- choose (0,abs size*2)
+  pure (thickness, size)
 
 
 main :: IO ()
